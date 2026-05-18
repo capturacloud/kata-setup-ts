6e4d980b6ba7b7c616c7362c1dba7a77944e4a1b
toni <tonigstm96@gmail.com>
2026-05-18 12:52:56 +0200

    REFACTOR: apply early return pattern

--- src/FizzBuzz.ts
+++ src/FizzBuzz.ts
@@ -2,7 +2,8 @@ export class FizzBuzz {
   fizzbuzz (input : number): string {
     if (input === 5) {
       return 'buzz';
-    } else if (input === 15){
+    }
+    if (input === 15) {
       return 'fizzbuzz';
     }
     return 'fizz';
