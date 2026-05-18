2dca706e786304a00d50ea0365c36f4b6cdd6056
toni <tonigstm96@gmail.com>
2026-05-18 12:43:06 +0200

    RED: should return buzz when 5 given

--- tests/FizzBuzz.spec.ts
+++ tests/FizzBuzz.spec.ts
@@ -4,4 +4,13 @@ it('should return fizz when 3 given', () => {
   const fizzbuzz = new FizzBuzz();
 
   expect(fizzbuzz.fizzbuzz(3)).toBe('fizz');
+
 });
+
+it('should return buzz when 5 given', () => {
+  const fizzbuzz = new FizzBuzz();
+
+  expect(fizzbuzz.fizzbuzz(5)).toBe('buzz');
+
+});
+
