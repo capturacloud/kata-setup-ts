5f5f5e891bef15a51f14b92386fc533b854d64e0
toni <tonigstm96@gmail.com>
2026-05-18 15:30:17 +0200

    RED: should return false when 1996 is the input

--- tests/LeapYear.spec.ts
+++ tests/LeapYear.spec.ts
@@ -5,3 +5,9 @@ it('should return false when 1997 is the input', () => {
 
   expect(input.leapYear(1997)).toBe(false);
 });
+
+it('should return true when 1996 is the input', () => {
+  const input = new LeapYear();
+
+  expect(input.leapYear(1996)).toBe(true);
+});
