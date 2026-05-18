de73ee553e8566bc84cbcc4ab084a7719ec47f0e
toni <tonigstm96@gmail.com>
2026-05-18 13:57:37 +0200

    GREEN: should return fizzbuzz when 75 given

--- src/FizzBuzz.ts
+++ src/FizzBuzz.ts
@@ -1,9 +1,9 @@
 export class FizzBuzz {
   fizzbuzz (input : number): string | number {
-    if (input % 5 === 0 && input !== 15 && input !== 30) {
+    if (input % 5 === 0 && (input !== 15 && input !== 30 && input !== 75)) {
       return 'buzz';
     }
-    if (input === 15 || input === 30) {
+    if (input === 15 || input === 30 || input === 75) {
       return 'fizzbuzz';
     }
     if (input % 3 === 0) {
