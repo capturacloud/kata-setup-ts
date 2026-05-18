f5a525dab8c7bae6a29e6520f0d975a973343e57
toni <tonigstm96@gmail.com>
2026-05-18 13:30:23 +0200

    REFACTOR: unify multiple 3

--- src/FizzBuzz.ts
+++ src/FizzBuzz.ts
@@ -6,7 +6,7 @@ export class FizzBuzz {
     if (input === 15) {
       return 'fizzbuzz';
     }
-    if (input === 3 || input === 6 || input === 42) {
+    if (input % 3 === 0) {
       return 'fizz';
     }
     return input;
