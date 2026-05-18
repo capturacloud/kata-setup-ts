850d08c894bd4fcebe54128c31c0b38bab0075c8
toni <tonigstm96@gmail.com>
2026-05-18 13:59:19 +0200

    REFACTOR: unify 3 and 5 multiple

--- src/FizzBuzz.ts
+++ src/FizzBuzz.ts
@@ -1,10 +1,10 @@
 export class FizzBuzz {
   fizzbuzz (input : number): string | number {
-    if (input % 5 === 0 && (input !== 15 && input !== 30 && input !== 75)) {
-      return 'buzz';
-    }
-    if (input === 15 || input === 30 || input === 75) {
+    if (input % 5 === 0 && input % 3 === 0) {
       return 'fizzbuzz';
+    }
+    if (input % 5 === 0) {
+      return 'buzz';
     }
     if (input % 3 === 0) {
       return 'fizz';
