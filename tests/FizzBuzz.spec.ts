eb5ad468d2f707c639e234daaa73fc731d820fcf
toni <tonigstm96@gmail.com>
2026-05-18 14:01:11 +0200

    REFACTOR: unify 3 and 5 multiple tests

--- tests/FizzBuzz.spec.ts
+++ tests/FizzBuzz.spec.ts
@@ -12,27 +12,11 @@ it.each([5, 10, 20]) (`should return buzz when input is a multiple of 5 (%i)` , 
   expect(fizzbuzz.fizzbuzz(input)).toBe('buzz');
 })
 
-
-it('should return fizzbuzz when 15 given', () => {
+it.each([15, 30, 75]) (`should return fizzbuzz when input is a multiple of 3 and 5 (%i)` , (input) => {
   const fizzbuzz = new FizzBuzz();
 
-  expect(fizzbuzz.fizzbuzz(15)).toBe('fizzbuzz');
-
-});
-
-it('should return fizzbuzz when 30 given', () => {
-  const fizzbuzz = new FizzBuzz();
-
-  expect(fizzbuzz.fizzbuzz(30)).toBe('fizzbuzz');
-
-});
-
-it('should return fizzbuzz when 75 given', () => {
-  const fizzbuzz = new FizzBuzz();
-
-  expect(fizzbuzz.fizzbuzz(75)).toBe('fizzbuzz');
-
-});
+  expect(fizzbuzz.fizzbuzz(input)).toBe('fizzbuzz');
+})
 
 it.each([1, 2, 43]) (`should return the given number (%i) when not a multiple of 3 or 5` , (input) => {
   const fizzbuzz = new FizzBuzz();
