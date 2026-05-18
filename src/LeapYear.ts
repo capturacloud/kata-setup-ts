1b98b20c45885b7b9f4d8af7905437731810b9f3
toni <tonigstm96@gmail.com>
2026-05-18 16:06:21 +0200

    GREEN: should return true when 2000 is the input

--- src/LeapYear.ts
+++ src/LeapYear.ts
@@ -1,6 +1,6 @@
 export class LeapYear {
   leapYear (input : number): boolean {
-    if (input === 1600) {
+    if (input === 1600 || input === 2000) {
       return true;
     }
     if (input % 100 === 0) {
