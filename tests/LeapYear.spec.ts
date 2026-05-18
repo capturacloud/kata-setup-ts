f5ae2e9dd6cc3566c44232cfcf1bece51492fe5f
toni <tonigstm96@gmail.com>
2026-05-18 15:55:55 +0200

    RED: should return false when 1700 is the input

--- tests/LeapYear.spec.ts
+++ tests/LeapYear.spec.ts
@@ -12,7 +12,7 @@ it.each([1996, 1992, 1988])( 'should return true when the input (%i) is divisibl
   expect(leap.leapYear(input)).toBe(true);
 });
 
-it.each([1800])( 'should return false when the input (%i) is divisible by 100', (input) => {
+it.each([1800, 1700])( 'should return false when the input (%i) is divisible by 100', (input) => {
   const leap = new LeapYear();
 
   expect(leap.leapYear(input)).toBe(false);
