124a3c23937a4ad248c5a9b0bc0be70a13dd530c
antonio-gg-dev <dev@antonio.gg>
2026-05-19 13:06:41 +0200

    refactor: group tests by method with describe

--- tests/Stack.spec.ts
+++ tests/Stack.spec.ts
@@ -1,65 +1,70 @@
 import { Stack } from '@/Stack'
 
-test('empty must return true on a newly created stack', () => {
-  const stack = new Stack();
+describe('empty()', () => {
+  test('empty must return true on a newly created stack', () => {
+    const stack = new Stack();
 
-  expect(stack.empty()).toBe(true);
-});
+    expect(stack.empty()).toBe(true);
+  });
 
-test('empty must return false on a non empty stack', () => {
-  const stack = new Stack();
-  stack.push('hello');
+  test('empty must return false on a non empty stack', () => {
+    const stack = new Stack();
+    stack.push('hello');
 
-  expect(stack.empty()).toBe(false);
-});
+    expect(stack.empty()).toBe(false);
+  });
 
-test('empty must return true when you pop all elements from the stack', () => {
-  const stack = new Stack();
-  stack.push('hello');
-  stack.pop();
+  test('empty must return true when you pop all elements from the stack', () => {
+    const stack = new Stack();
+    stack.push('hello');
+    stack.pop();
 
-  expect(stack.empty()).toBe(true);
-});
+    expect(stack.empty()).toBe(true);
+  });
+})
 
-test('peek must return null on a newly created stack', () => {
-  const stack = new Stack();
+describe('peek()', () => {
+  test('peek must return null on a newly created stack', () => {
+    const stack = new Stack();
 
-  expect(stack.peek()).toBe(null);
-});
+    expect(stack.peek()).toBe(null);
+  });
 
-test('peek must return the last element in the stack', () => {
-  const stack = new Stack();
-  stack.push('hello');
+  test('peek must return the last element in the stack', () => {
+    const stack = new Stack();
+    stack.push('hello');
 
-  expect(stack.peek()).toBe('hello');
-});
+    expect(stack.peek()).toBe('hello');
+  });
+})
 
-test('size must return 0 on a newly created stack', () => {
+describe('size()', () => {test('size must return 0 on a newly created stack', () => {
   const stack = new Stack();
 
   expect(stack.size()).toBe(0);
 });
 
-test('size must return 1 when you push one element', () => {
-  const stack = new Stack();
-  stack.push('hello');
+  test('size must return 1 when you push one element', () => {
+    const stack = new Stack();
+    stack.push('hello');
 
-  expect(stack.size()).toBe(1);
-});
+    expect(stack.size()).toBe(1);
+  });
 
-test('size must return 2 when you push two elements', () => {
-  const stack = new Stack();
-  stack.push('hello');
-  stack.push('world');
+  test('size must return 2 when you push two elements', () => {
+    const stack = new Stack();
+    stack.push('hello');
+    stack.push('world');
 
-  expect(stack.size()).toBe(2);
-});
+    expect(stack.size()).toBe(2);
+  });
 
-test('size must return the amount of elements in the stack', () => {
-  const stack = new Stack();
-  stack.push('hello');
-  stack.push('world');
-  stack.pop();
+  test('size must return the amount of elements in the stack', () => {
+    const stack = new Stack();
+    stack.push('hello');
+    stack.push('world');
+    stack.pop();
 
-  expect(stack.size()).toBe(1);
-});
+    expect(stack.size()).toBe(1);
+  });
+})
