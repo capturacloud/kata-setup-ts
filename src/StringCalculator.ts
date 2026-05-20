122488eae02a4d4c439c61ca15914e501bbe5193
toni <tonigstm96@gmail.com>
2026-05-20 16:21:38 +0200

    GREEN: should return 4 when passed a string with 4

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,6 +1,9 @@
 export class StringCalculator {
 
   add(numbers: string): number {
+    if (numbers === '4') {
+      return 4;
+    }
     return 0;
   }
 }
--- tests/StringCalculator.spec.ts
+++ tests/StringCalculator.spec.ts
@@ -5,3 +5,9 @@ it('should return zero when passed and empty string', () => {
 
   expect(calculator.add('')).toBe(0);
 });
+
+it('should return 4 when passed a string with "4"', () => {
+  const calculator = new StringCalculator();
+
+  expect(calculator.add('4')).toBe(4);
+});
