1e83aac48d9486ca7649e83547bab00fa0bcc370
Raúl Pavón <raulpavonfp@gmail.com>
2026-05-20 18:18:54 +0200

    GREEN: should return 2 with a roll of 2

--- src/Bowling.ts
+++ src/Bowling.ts
@@ -1,5 +1,12 @@
 export class Bowling {
+  private _rolled: boolean = false;
+  roll(pins: number): void {
+    this._rolled = true;
+  }
   score():number {
+    if (this._rolled) {
+      return 2
+    }
     return 0;
   }
 }
--- tests/Bowling.spec.ts
+++ tests/Bowling.spec.ts
@@ -5,3 +5,9 @@ it('should return 0 if there is no rolls', () => {
 
   expect(bowling.score()).toBe(0);
 });
+
+it('should return 2 with a roll of 2', () => {
+  const bowling = new Bowling()
+  bowling.roll(2)
+  expect(bowling.score()).toBe(2)
+})
