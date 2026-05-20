797477d5fa845f9c24f7c6c8e924e908a24fdff5
toni <tonigstm96@gmail.com>
2026-05-20 19:28:00 +0200

    REFACTOR: should return 3 when passed a string with //-\n1-2

--- tests/StringCalculator.spec.ts
+++ tests/StringCalculator.spec.ts
@@ -57,6 +57,6 @@ it('should return 3 when passed a string with "//;\\n1;2"', () => {
 it('should return 3 when passed a string with "//-\\n1-2"', () => {
   const calculator = new StringCalculator();
 
-  expect(calculator.add('//-\\n1;2')).toBe(3);
+  expect(calculator.add('//-\\n1-2')).toBe(3);
 });
 
