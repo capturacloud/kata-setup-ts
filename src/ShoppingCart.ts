b39db480a6f082fba758fff0896ae163e4152abf
toni <tonigstm96@gmail.com>
2026-06-01 14:43:29 +0200

    refactor: improved print method logic

--- src/ShoppingCart.ts
+++ src/ShoppingCart.ts
@@ -1,54 +1,12 @@
-import {Product} from "@/Product";
 import {ProductItem} from "@/ProductItem";
 
 export class ShoppingCart {
-  added: boolean = false;
-  numberAdded: number = 0;
+  added : boolean = false;
+  productList: ProductItem[] = [];
 
   print (): string {
-    if (this.numberAdded === 3) {
+    if (!this.added) {
       return `
- --------------------------------------------
- | Producto     | Precio con IVA | Cantidad |
- | TMNT Booster | 5.00€          |        1 | 
- | Penny Sleeves| 1.00€          |        1 |
- | Dice Set     | 3.00€          |        1 |
- |------------------------------------------|
- | Promoción:                               |
- --------------------------------------------
- | Total de productos: 3                    |
- | Precio total: 9.00 €                     |
- --------------------------------------------
-`
-    }
-    if (this.numberAdded > 1) {
-      return `
- --------------------------------------------
- | Producto     | Precio con IVA | Cantidad |
- | TMNT Booster | 5.00€          |        1 | 
- | Penny Sleeves| 1.00€          |        1 |
- |------------------------------------------|
- | Promoción:                               |
- --------------------------------------------
- | Total de productos: 2                    |
- | Precio total: 6.00 €                     |
- --------------------------------------------
-`
-    }
-    if (this.added) {
-      return `
- --------------------------------------------
- | Producto     | Precio con IVA | Cantidad |
- | TMNT Booster | 5.00€          |        1 |
- |------------------------------------------|
- | Promoción:                               |
- --------------------------------------------
- | Total de productos: 1                    |
- | Precio total: 5.00 €                     |
- --------------------------------------------
-`
-    }
-    return `
  --------------------------------------------
  | Producto     | Precio con IVA | Cantidad |
  | -----------  | -------------- | -------- |
@@ -58,11 +16,43 @@ export class ShoppingCart {
  | Total de productos: 0                    |
  | Precio total: 0.00 €                     |
  --------------------------------------------
-`;
-  }
+`
+    }
 
-  add(product: ProductItem) {
+    let productString: string = ''
+    let totalPrice: number = 0;
+    let totalProducts: number = 0;
+
+    for (const product of this.productList) {
+      let name = product.product.name;
+      let price = product.product.price;
+      let quantity = product.quantity;
+
+      totalPrice += (price * quantity);
+      totalProducts += quantity;
+
+      const paddedName = name.padEnd(12, ' ');
+      const paddedPrice = `${price.toFixed(2)}€`.padEnd(14, ' ');
+      const paddedQuantity = quantity.toString().padStart(8, ' ');
+
+        productString += ` | ${paddedName} | ${paddedPrice} | ${paddedQuantity} |\n`;
+    }
+
+      const paddedTotalProducts = totalProducts.toString().padEnd(20, ' ');
+      const paddedTotalPrice = `${totalPrice.toFixed(2)} €`.padEnd(26);
+      return `
+ --------------------------------------------
+ | Producto     | Precio con IVA | Cantidad |
+${productString} |------------------------------------------|
+ | Promoción:                               |
+ -------------------------------------------|
+ | Total de productos: ${paddedTotalProducts} |
+ | Precio total: ${paddedTotalPrice} |
+ --------------------------------------------
+`}
+
+  add(...product: ProductItem[]) {
     this.added = true;
-    this.numberAdded += 1;
+    this.productList.push(...product);
   }
 }
--- tests/ShoppingCart.spec.ts
+++ tests/ShoppingCart.spec.ts
@@ -30,7 +30,7 @@ it('print should print a cart with one booster when a booster is added', () => {
  | TMNT Booster | 5.00€          |        1 |
  |------------------------------------------|
  | Promoción:                               |
- --------------------------------------------
+ -------------------------------------------|
  | Total de productos: 1                    |
  | Precio total: 5.00 €                     |
  --------------------------------------------
@@ -39,20 +39,19 @@ it('print should print a cart with one booster when a booster is added', () => {
   it('print should print a cart with one booster and one sleeve when a booster and a sleeve is added', () => {
     const cart = new ShoppingCart()
     const booster = new Product('TMNT Booster', 5.00, 21, 15)
-    const sleeve = new Product('UltraPRO Penny Sleeves', 1.00, 14, 12)
+    const sleeve = new Product('Sleeves', 1.00, 14, 12)
     const boosterItem = new ProductItem(1, booster)
     const sleeveItem = new ProductItem(1, sleeve)
-    cart.add(boosterItem)
-    cart.add(sleeveItem)
+    cart.add(boosterItem, sleeveItem)
 
     expect(cart.print()).toBe(`
  --------------------------------------------
  | Producto     | Precio con IVA | Cantidad |
- | TMNT Booster | 5.00€          |        1 | 
- | Penny Sleeves| 1.00€          |        1 |
+ | TMNT Booster | 5.00€          |        1 |
+ | Sleeves      | 1.00€          |        1 |
  |------------------------------------------|
  | Promoción:                               |
- --------------------------------------------
+ -------------------------------------------|
  | Total de productos: 2                    |
  | Precio total: 6.00 €                     |
  --------------------------------------------
@@ -62,24 +61,22 @@ it('print should print a cart with one booster when a booster is added', () => {
 it('print should print a cart with one booster, one sleeve and one dice when a booster, a sleeve and a dice is added', () => {
   const cart = new ShoppingCart()
   const booster = new Product('TMNT Booster', 5.00, 21, 15)
-  const sleeve = new Product('UltraPRO Penny Sleeves', 1.00, 14, 12)
-  const diceSet = new Product('Temu Dice Set', 3.00, 14, 12)
+  const sleeve = new Product('Sleeves', 1.00, 14, 12)
+  const diceSet = new Product('Dice Set', 3.00, 14, 12)
   const boosterItem = new ProductItem(1, booster)
   const sleeveItem = new ProductItem(1, sleeve)
   const diceSetItem = new ProductItem(1, diceSet)
-  cart.add(boosterItem)
-  cart.add(sleeveItem)
-  cart.add(diceSetItem)
+  cart.add(boosterItem, sleeveItem, diceSetItem)
 
   expect(cart.print()).toBe(`
  --------------------------------------------
  | Producto     | Precio con IVA | Cantidad |
- | TMNT Booster | 5.00€          |        1 | 
- | Penny Sleeves| 1.00€          |        1 |
+ | TMNT Booster | 5.00€          |        1 |
+ | Sleeves      | 1.00€          |        1 |
  | Dice Set     | 3.00€          |        1 |
  |------------------------------------------|
  | Promoción:                               |
- --------------------------------------------
+ -------------------------------------------|
  | Total de productos: 3                    |
  | Precio total: 9.00 €                     |
  --------------------------------------------
